48d38e6689b86aa5a9acb4627563b51e8a3ebd0a
TJ <tj@ojingolabs.com>
2012-08-04 18:04:50 +0400

    more tweaking adding existential operator and heredocs stuff

--- coffee/sayHi.coffee
+++ coffee/sayHi.coffee
@@ -1,3 +1,9 @@
+### my block comment test!
+	this will leave all special commetary in the code
+	for later use
+		TRIPLE POUND FOR BLOCK COMMENTS IN PLACE!
+###
+
 # simple @ use
 @sayhi = ->
 	console.log("Hello with an @ sign used!")
@@ -24,4 +30,25 @@ console.log text
 # call a conditional within the #{} and allow choice based on solving for it
 day = 'Monday'
 console.log "Its a beautiful #{if day is 'Sunday' then day else "DAY!"}"
+
+# show how you can use triple quotes to do a heredocs wit TRIPLE QUOTES!
+
+field2 = """
+		<ul>
+			<li>
+				<input type ='text' name='#{ someName}' id='#{someId}' value='#{escape someValue}'>"
+			</li>
+		</ul>
+		"""
+console.log field2
+
+# existential operator
+
+console.log "Does x exist?\n"
+console.log x?
+console.log "Does text exist?\n"
+console.log text?
+
+console.log(doIexist?)
+
 
--- js/sayHi.js
+++ js/sayHi.js
@@ -1,5 +1,13 @@
+
+/* my block comment test!
+	this will leave all special commetary in the code
+	for later use
+		TRIPLE POUND FOR BLOCK COMMENTS IN PLACE!
+*/
+
+
 (function() {
-  var day, field, someId, someName, someValue, text;
+  var day, field, field2, someId, someName, someValue, text;
 
   this.sayhi = function() {
     return console.log("Hello with an @ sign used!");
@@ -26,5 +34,19 @@
   day = 'Monday';
 
   console.log("Its a beautiful " + (day === 'Sunday' ? day : "DAY!"));
+
+  field2 = "<ul>\n	<li>\n		<input type ='text' name='" + someName + "' id='" + someId + "' value='" + (escape(someValue)) + "'>\"\n	</li>\n</ul>";
+
+  console.log(field2);
+
+  console.log("Does x exist?\n");
+
+  console.log(typeof x !== "undefined" && x !== null);
+
+  console.log("Does text exist?\n");
+
+  console.log(text != null);
+
+  console.log(typeof doIexist !== "undefined" && doIexist !== null);
 
 }).call(this);
